4a3c1e486ef00466e699cfd661f3e12973930408
Prajeet SparkBrain <prajeet@sparkbrainai.com>
2026-04-05 15:01:55 +0545

    config update

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,67 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { PORT } from "./config";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { BadRequestException, ValidationPipe } from "@nestjs/common";
-import { HttpExceptionFilter } from "./common/exceptions/error.exception";
-import { join } from "path";
-import * as compression from "compression";
-import helmet from "helmet";
-import { GraphQLError } from "graphql";
+import 'tsconfig-paths/register'; // resolves src/* imports
 
-async function bootstrap() {
-  const appOptions = { cors: true };
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    appOptions,
-  );
-  app.enableCors({
-    origin: [
-      "https://ridehailing.com", // your production domain
-      "https://www.ridehailing.com", // with www if applicable
-      "http://localhost:3000", // for local frontend testing
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "Accept",
-      "Origin",
-    ],
-    credentials: true, // if you send cookies or Authorization headers
-  });
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express';
+import { HttpExceptionFilter } from './common/exceptions/error.exception';
+import { join } from 'path';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import express from 'express';
+// use require to avoid missing type declarations for @vendia/serverless-express
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const serverlessExpress: any = require('@vendia/serverless-express');
 
-  app.setGlobalPrefix("api");
-  app.useGlobalFilters(new HttpExceptionFilter());
+const server = express();
+let cachedServer: any;
 
-  app.use(
-    helmet({
+async function bootstrapServerless() {
+  if (!cachedServer) {
+    const app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+      new ExpressAdapter(server),
+    );
+
+    // CORS
+    app.enableCors({
+      origin: [
+        'https://ridehailing.com',
+        'https://www.ridehailing.com',
+        'http://localhost:3000',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+      credentials: true,
+    });
+
+    // Global prefix & filters
+    app.setGlobalPrefix('api');
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    // Security & performance
+    app.use(helmet({
       contentSecurityPolicy: false,
       crossOriginEmbedderPolicy: false,
-    }),
-  );
-  app.use(compression());
+    }));
+    app.use(compression());
 
-  app.useStaticAssets(join(__dirname, "..", "files"), {
-    prefix: "/files/",
-  });
+    // Static files
+    app.useStaticAssets(join(__dirname, '..', 'files'), {
+      prefix: '/files/',
+    });
 
-  await app.listen(PORT);
+    // Init app
+    await app.init();
+
+    // Wrap with serverless
+    cachedServer = serverlessExpress({ app: server });
+  }
+
+  return cachedServer;
 }
-bootstrap();
+
+// Vercel handler
+export default async function handler(req: any, res: any) {
+  const server = await bootstrapServerless();
+  return server(req, res);
+}
