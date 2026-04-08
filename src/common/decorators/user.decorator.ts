9f8dfb96210c870c1f0545a5553abd3acb7959b6
Anil <anilbhomi201@gmail.com>
2026-04-07 21:40:40 -0400

    fix refresh token issue

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -1,8 +1,12 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
 
-export const CurrentUser = createParamDecorator((data: string, context: ExecutionContext) => {
-  const request = context.switchToHttp().getRequest();
-  const user = request.user;
-  return data ? user && user[data] : user;
-});
+export const CurrentUser = createParamDecorator(
+  (data: string, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    const request = ctx.getContext().req;
+    const user = request?.user;
 
+    return data ? user?.[data] : user;
+  },
+);
