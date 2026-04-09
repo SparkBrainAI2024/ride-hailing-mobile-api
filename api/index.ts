be20736eea3cb5c260d989f87baba9375780723e
Prajeet SparkBrain <prajeet@sparkbrainai.com>
2026-04-09 08:59:55 +0545

    some more changes

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,26 @@
-// api/index.ts
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import { AppModule } from '../src/app.module';
+import * as express from 'express';
 
-const server = express();
+const expressApp = express();
+let isInitialized = false;
 
-export const config = {
-  api: { bodyParser: true },
-};
-
-let app: any;
-
-async function bootstrap() {
-  if (!app) {
-    app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+const bootstrap = async () => {
+  if (!isInitialized) {
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+      { logger: false }
+    );
     app.enableCors();
     await app.init();
+    isInitialized = true;
   }
-  return app;
-}
+  return expressApp;
+};
 
 export default async (req: any, res: any) => {
-  await bootstrap();
+  const server = await bootstrap();
   server(req, res);
 };
